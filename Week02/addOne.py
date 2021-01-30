2aba707d61620a594d7fdb05eca0c26a83d19feb
NiallOFGalway <G00398819@gmit.ie>
2021-01-30 14:10:42 +0000

    Week 02

---
+++ Week02/addOne.py
@@ -0,0 +1,14 @@
+# addOne.py
+# Author: Niall O Flaherty
+# In this program, when you enter a number, the output will be that number +1
+# Reference: Adrew Beatty / VIDEO:  Prog 2.2.1 First Programs
+
+number = int(input('Enter Number'))
+# This asks the user to input a number, hence INPUT. However, we must convert this to an integer
+# so that it can be added below.pyth
+
+newNumber = number + 1
+# This defines that the output will be the original number +1
+
+print ('{} plus one is {}'.format(number,newNumber))
+# This outputs the original numer + 1
